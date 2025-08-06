665688b03bfd132b88b778e26245486c2ad8bce9
aindi-portfolio <aindi.tsarni@gmail.com>
2025-08-06 10:03:27 -0400

    created monster, player, user apps. will add additional models withouth much depth to avoid errors. Then will create registration and authentication.

---
+++ Adventurers_Ledger_Proj/api_playground.py
@@ -0,0 +1,46 @@
+import requests
+from dotenv import load_dotenv
+load_dotenv('.env')  # Load environment variables from .env file
+import json
+import os
+from django.core.cache import cache
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+
+RAWG_API_KEY = os.environ.get("MY_RAWG_KEY")
+# fetche names and release dates of the top 5 games.
+url = f'https://api.rawg.io/api/games?key={RAWG_API_KEY}&page_size=5'
+# TERMS OF SERVICE:
+# Free for personal use as long as you attribute RAWG as the source of the data 
+# and/or images and add an active hyperlink from every page where the data of RAWG is used.
+
+
+class CachedDataAPIView(APIView):
+    def get(request=None, pk='top_5'):
+        cache_key = f'RAWG_{pk}'
+        # print(cache.get(cache_key))
+        data = cache.get(cache_key)
+        if not data:
+            # print("-----------inside the 'if not data' loop----------")
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                # for game in data['results']:
+                #     print(f"{game['name']} - Released: {game['released']}")
+                cache.set(cache_key, data['results'], timeout=60*10) # will hold the data for 60 sec * 10 = 10 min
+            else:
+                print("Failed to fetch: ", response.status_code)
+        # print("-----------outside the 'if not data' loop----------")
+        # print(data)
+        return data
+    
+    def put(self, request): # maybe a third param
+        """
+        Update the cache with new data from the API.
+        """
+        cache_key = f'RAWG_top_5'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            cache.set(cache_key, data['results'], timeout=60*10)
---
+++ Adventurers_Ledger_Proj/monster_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Monster(models.Model):
+    """
+    This model represents a monster in the game.
+    """
+    name = models.CharField(max_length=100, unique=True)
+    base_health = models.IntegerField(default=100)
+    base_damage = models.IntegerField(default=10)
+    element_type = models.CharField(max_length=50, choices=['fire', 'water', 'normal', 'air'], default='normal')
+    image_url = models.URLField(max_length=200, blank=True, null=True)
