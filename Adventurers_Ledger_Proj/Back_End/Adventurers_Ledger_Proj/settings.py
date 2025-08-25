d5d2add7c47150d53ec10a9c6805fc248957688a
aindi-portfolio <aindi.tsarni@gmail.com>
2025-08-25 11:57:04 -0400

    DEBUG=False in settings.py

--- Adventurers_Ledger_Proj/Back_End/Adventurers_Ledger_Proj/settings.py
+++ Adventurers_Ledger_Proj/Back_End/Adventurers_Ledger_Proj/settings.py
@@ -31,7 +31,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # Load environment variabl
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
