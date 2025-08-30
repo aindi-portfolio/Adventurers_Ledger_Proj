e142ff4f29107766199ded853bfba15f235d9d5b
aindi-portfolio <aindi.tsarni@gmail.com>
2025-08-30 11:28:36 -0400

    the app now works in my local environment

--- Adventurers_Ledger_Proj/Back_End/Adventurers_Ledger_Proj/settings.py
+++ Adventurers_Ledger_Proj/Back_End/Adventurers_Ledger_Proj/settings.py
@@ -96,11 +96,7 @@ WSGI_APPLICATION = 'Adventurers_Ledger_Proj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'adventurers_db'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': 'adventurers_db',
     }
 }
 
